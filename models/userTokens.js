97cc2d9b90a08937bb02fc36855deb867fec8e45
Kouroshnj <k.nejatian.gh@gmail.com>
2023-10-01 12:36:26 +0330

    removed comments.

--- models/userTokens.js
+++ models/userTokens.js
@@ -24,8 +24,3 @@ const userTokens = mongoose.model("userTokens", tokenSchema)
 userTokens.collection.createIndex({ expireAt: 1 }, { expireAfterSeconds: environmentExp })
 
 module.exports = userTokens
-
-//1696061794
-//1696062525325
-//1696061556621
-//
