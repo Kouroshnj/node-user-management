de3d735111dd0c82fb76c3bbc75dbdda915478a4
Kouroshnj <k.nejatian.gh@gmail.com>
2023-10-16 18:44:04 +0330

    added new base methods to base service

--- src/services/base.service.js
+++ src/services/base.service.js
@@ -29,6 +29,22 @@ class BaseService {
     async findOneAndUpdate(query, operation) {
         return this.model.findOneAndUpdate(query, operation)
     }
+
+    async findOneAndDelete(query, operation) {
+        return this.model.findOneAndDelete(query, operation)
+    }
+
+    async findAndModify(query, operation) {
+        return this.model.findAndModify(query, operation)
+    }
+
+    async findOneAndReplace(query, operation) {
+        return this.model.findOneAndReplace(query, operation)
+    }
+
+    async aggregate(pipeline) {
+        return this.model.aggregate(pipeline)
+    }
 }
 
 module.exports = BaseService
